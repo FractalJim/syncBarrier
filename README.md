4c06deeb18041cfcbf4d37582694cdefaff71819
FractalJim <james_matheson@yahoo.com>
2015-06-26 21:32:11 +1000

    Adding code files and docs.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # syncBarrier
-Node module for creating synchronisation barrier to enable waiting for several async functions to complete bedore proceeding
+Node module for creating a synchronisation barrier that waits for several async function calls to complete before running another function.
+
+There are three steps to using syncBarrier :
+1. Create a new barrier object and pass it the function you want to run on completion (the onComplete function) and any arguments that function needs.
+2. For each function you want to finish running before the onComplete function is called  call "registerFunction" on the barrier.  A completion callback must be the final argument in the argument list of the function you are registering. You should not pass this callback yourself as the barrier will pass a callback of its own. The function you register will be called with the arguments you pass "registerFunction" as soon as you register it.
+3. Once you have registered all the functions you want to wait for, call "startWaitComplete" on the barrier object. The onComplete function will then run as soon as all the registered functions have finished running.
+
+## Example
+This example (taken from the test code) use two barriers.  It will print the output from the barrier2 functions followed by the output
+from the barrier1  functions (as the functions used with barrier2 are actually synchronus, so get executed immediately).
+Notice that  the final argument to the test functions is a callback.  
+
+        //Should see output of inline functiojn calls (barrier2) then Asycnh calls (barrier1) and complete notifications
+        function test ()
+        {
+            //Asynch function call
+             var barrier1 = new Barrier(function(){console.log("Complete 1")})
+             barrier1.registerFunction(testFunc,4);
+             barrier1.registerFunction(testFunc,6);
+             barrier1.startWaitComplete();
+             //Inline function calls
+             var barrier2 = new Barrier(function(){console.log("Complete 2")})
+             barrier2.registerFunction(testFuncBody,7);
+             barrier2.registerFunction(testFuncBody,9);
+             barrier2.startWaitComplete();
+        }
+
+         function testFunc(num,end)
+         {
+             setTimeout(testFuncBody,10,num,end);
+
+         }
+
+         function testFuncBody(num,end)
+         {
+             for (var i=0;i<num;i++)
+               {
+                 console.log(i +"/" + num);
+               }
+              end();
+         }
---
+++ syncBarrier.js
@@ -0,0 +1,133 @@
+/*
+syncBarrier: This module provides a simple synchronisation barrier
+for use with nodejs to allow the running of a function after two or more
+other asynchronos calls have completed.
+
+Copyright 2015 James W. Matheson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+if (require.main === module)
+{
+     console.log("Running synchBarrier Test");
+     test();
+} else
+{
+    module.exports.Barrier = Barrier;
+}
+
+//Should see output of inline functiojn calls (barrier2) then Asycnh calls (barrier1) and complete notifications
+function test ()
+{
+    //Asynch function call
+     var barrier = new Barrier(function(){console.log("Complete 1")})
+     barrier.registerFunction(testFunc,4);
+     barrier.registerFunction(testFunc,6);
+     barrier.startWaitComplete();
+     //Inline function calls
+     var barrier2 = new Barrier(function(){console.log("Complete 2")})
+     barrier2.registerFunction(testFuncBody,7);
+     barrier2.registerFunction(testFuncBody,9);
+     barrier2.startWaitComplete();
+}
+
+ function testFunc(num,end)
+ {
+     setTimeout(testFuncBody,10,num,end);
+
+ }
+
+ function testFuncBody(num,end)
+ {
+     for (var i=0;i<num;i++)
+       {
+         console.log(i +"/" + num);
+       }
+      end();
+ }
+
+
+function Barrier(doneFunction)
+{
+    this.functionCount = 0;
+    this.waiting = false;
+    this.allDoneFunction = doneFunction
+    this.oneDoneFunction = oneDoneFunctionClosure(this);
+    this.startWaitComplete = startWaitComplete;
+    this.registerFunction = registerFunction;
+
+    //not using slice function as is recommended against on MDN arguments article.
+    if (arguments.length===1)
+    {
+           this.allDoneArgs = [];
+    } else
+    {
+        var argArray = Array(arguments.length-1);
+        for ( var i = 1; i < (arguments.length);i++)
+        {
+              argArray[i-1] = arguments[i];
+        }
+        this.allDoneArgs= argArray;
+   }
+}
+
+//expect first argument is function subsequent arguments are args for function excluding call back argument
+function registerFunction()
+{
+        if(this.waiting===true)
+        {
+                throw "SyncBarrier: You may not regsiter a function once you have started waiting for completion";
+        }
+
+        var funcToCall = arguments[0];
+        //convert arguments to array and take from 1 to end
+
+        //not using slice function as is recommended against on MDN arguments article.
+        var argArray = Array(arguments.length);
+        for ( var i = 1; i < (arguments.length);i++)
+        {
+              argArray[i-1] = arguments[i];
+        }
+        argArray[arguments.length-1] = this.oneDoneFunction;
+        this.functionCount++;
+
+         funcToCall.apply(null, argArray);
+
+}
+
+function startWaitComplete()
+{
+      if (this.functionCount===0)
+      {
+           this.allDoneFunction.apply(null,this.allDoneArgs);
+      } else {
+         this.waiting=true;
+      }
+
+}
+
+
+function oneDoneFunctionClosure(selfRef)
+{
+        return(function ()  {
+            selfRef.functionCount--;
+            if (selfRef.waiting)
+            {
+                 if (selfRef.functionCount===0)
+                  {
+                       selfRef.allDoneFunction.apply(null,selfRef.allDoneArgs);
+                  }
+            }
+      });
+}
