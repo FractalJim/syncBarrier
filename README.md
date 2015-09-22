224a03696797c93471cbf3bed32fdb0d77f44b9a
James <james_matheson@yahoo.com>
2015-09-22 22:47:03 +1000

    Breaking Change (not compatible with version 1) Added ability to set execution context for functions (this) Added better unit tests Refactored

--- README.md
+++ README.md
@@ -1,50 +1,57 @@
 # syncBarrier
 Node module for creating a synchronisation barrier that waits for several async function calls to complete before running another function.
 
+Version 2 (breaking change) introduces a new second argument to the constructor and registration functions to specify the object that should be used as this when executing these functions.  
+
 There are three steps to using syncBarrier :
 
-1. Create a new barrier object and pass it the function you want to run on completion (the onComplete function) and any arguments that function needs.
+1. Create a new barrier object and pass it the function you want to run on completion (the onComplete function), a reference to be used for "this" while running the function (can be null)  and any arguments that function needs.
 
-2. For each function you want to finish running before the onComplete function is called  call "registerFunction" on the barrier.  A completion callback must be the final argument in the argument list of the function you are registering. You should not pass this callback yourself as the barrier will pass a callback of its own. The function you register will be called with the arguments you pass "registerFunction" as soon as you register it.
+2. For each function you want to finish running before the onComplete function is called  call "registerFunction" on the barrier passing argumetns in the same manner as to the constructor.  A completion callback must be the final argument in the argument list of the function you are registering. You should not pass this callback yourself as the barrier will pass a callback of its own. The function you register will be called with the arguments you pass "registerFunction" as soon as you register it.
 
 3. Once you have registered all the functions you want to wait for, call "startWaitComplete" on the barrier object. The onComplete function will then run as soon as all the registered functions have finished running.
 
 ## Example
-This example (taken from the test code) use two barriers.  It will print the output from the barrier2 functions followed by the output
-from the barrier1  functions (as the functions used with barrier2 are actually synchronus, so get executed immediately).
+This example (taken from the test code) uses two barriers.  It will print the output from the barrier2 functions followed by the output
+from the barrier1  functions (as the functions used with barrier2 are actually synchronous, so get executed immediately).
 Notice that  the final argument to the test functions is a callback.  
 ```JavaScript
 var syncBarrier = require('syncbarrier')
 
-//Should see output of inline function calls (barrier2) then async calls (barrier1) and complete notifications
+//Should see output like
+/*
+3
+4
+Barrier 2 Complete
+1
+2
+Barrier 1 Complete
+*/
 test()
 
 function test ()
 {
-        //Asynch function call
-        var barrier1 = new syncBarrier.Barrier(function(){console.log("Complete 1")})
-        barrier1.registerFunction(testFunc,4);
-        barrier1.registerFunction(testFunc,6);
-        barrier1.startWaitComplete();
+        var barrier = new Barrier(testComplete,null,"Barrier 1 Complete",testArray,testIndex);
+        barrier.registerFunction(testFunction,null,1);
+        barrier.registerFunction(testFunction,null,2);
+        barrier.startWaitComplete();
 
         //Inline function calls
-        var barrier2 = new syncBarrier.Barrier(function(){console.log("Complete 2")})
-        barrier2.registerFunction(testFuncBody,7);
-        barrier2.registerFunction(testFuncBody,9);
+        var barrier2 = new Barrier(console.log,null,"Barrier 2 Complete");
+        barrier2.registerFunction(testFunctionSync,null,3);
+        barrier2.registerFunction(testFunctionSync,null,4);
         barrier2.startWaitComplete();
 }
 
-function testFunc(num,end)
+function testFunction(num,end)
 {
-        setTimeout(testFuncBody,10,num,end);
+  setTimeout(testFunctionSync,10,num,end);
 }
 
-function testFuncBody(num,end)
+function testFunctionSync(num,end)
 {
-        for (var i=0;i<num;i++)
-        {
-             console.log(i +"/" + num);
-        }
-        end();
+  console.log(num);
+  end();
 }
+
 ```
--- syncBarrier.js
+++ syncBarrier.js
@@ -18,116 +18,181 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
+var barrierArgumentCount =2;
+
+function Barrier(onCompleteFunction,onCompleteObject)
+{
+  this.functionCount = 0;
+  this.waiting = false;
+  this.allFunctionsDone = onCompleteFunction;
+  this.allFunctionsDoneObject = onCompleteObject;
+
+  if (arguments.length <= barrierArgumentCount)
+  {
+    this.allDoneArgs = [];
+  } else
+  {
+    this.allDoneArgs = createFunctionArgumentsArray(arguments);
+  }
+}
+
+Barrier.prototype.registerFunction = function(targetFunction,targetObject)
+{
+  if(this.waiting===true)
+  {
+    throw "SyncBarrier: You may not regsiter a function once you have started waiting for completion";
+  }
+
+  //convert arguments to array and take from 1 to end
+  var argArray = createFunctionArgumentsArray(arguments);
+  argArray[argArray.length] = oneFunctionDoneClosure(this);
+  this.functionCount++;
+
+  targetFunction.apply(targetObject, argArray);
+
+}
+
+Barrier.prototype.startWaitComplete = function ()
+{
+  if (this.functionCount===0)
+  {
+    this.allFunctionsDone.apply(this.allFunctionsDoneObject,this.allDoneArgs);
+  } else {
+    this.waiting=true;
+  }
+
+}
+
+function oneFunctionDoneClosure(self)
+{
+
+  return( function()
+  {
+    self.functionCount--;
+    if (self.waiting)
+    {
+      if (self.functionCount===0)
+      {
+        self.allFunctionsDone.apply(self.allFunctionsDoneObject,self.allDoneArgs);
+      }
+    }
+
+  });
+}
+
+function createFunctionArgumentsArray(argumentObject)
+{
+  //not using slice function as arguments object is not a full blooded array.
+  var argArray = Array(argumentObject.length-barrierArgumentCount);
+  for ( var i = barrierArgumentCount; i < (argumentObject.length);i++)
+  {
+        argArray[i-barrierArgumentCount] = argumentObject[i];
+  }
+  return argArray;
+}
+
 if (require.main === module)
 {
-     console.log("Running synchBarrier Test");
-     test();
+  console.log("Running synchBarrier Test");
+  test();
 } else
 {
-    module.exports.Barrier = Barrier;
+  module.exports.Barrier = Barrier;
 }
 
-//Should see output of inline functiojn calls (barrier2) then Asycnh calls (barrier1) and pass notifications
+// <test>
+
 function test ()
 {
-    //Asynch function call
-     var barrier = new Barrier(function(){console.log("Passed 1")})
-     barrier.registerFunction(testFunc,4);
-     barrier.registerFunction(testFunc,6);
-     barrier.startWaitComplete();
-     //Inline function calls
-     var barrier2 = new Barrier(function(){console.log("Passed 2")})
-     barrier2.registerFunction(testFuncBody,7);
-     barrier2.registerFunction(testFuncBody,9);
-     barrier2.startWaitComplete();
+
+  var tests=[];
+  tests[0] = function(testArray,testIndex) {
+    console.log("Test 1: No arguments for registered functions\n Should print 0 twice on seperate lines.");
+    var barrier = new Barrier(testComplete,null,"Test 1 Complete",testArray,testIndex);
+    barrier.registerFunction(testNoFunctionArguments,null);
+    barrier.registerFunction(testNoFunctionArguments,null);
+    barrier.startWaitComplete();
+  }
+
+  tests[1] = function(testArray,testIndex) {
+  console.log("\nTest 2: Multiple barriers with arguments for registered functions\n Should print (all on seperate lines) 3, 4, 'Barrier 2 Complete'  and then 1,2 or 2,1, 'Barrier 1 Complete'\nTest results below.");
+  //Delayed (Asynch) Calls
+  var barrier = new Barrier(testComplete,null,"Barrier 1 Complete",testArray,testIndex);
+  barrier.registerFunction(testFunction,null,1);
+  barrier.registerFunction(testFunction,null,2);
+  barrier.startWaitComplete();
+  //Inline function calls
+  var barrier2 = new Barrier(console.log,null,"Barrier 2 Complete");
+  barrier2.registerFunction(testFunctionSync,null,3);
+  barrier2.registerFunction(testFunctionSync,null,4);
+  barrier2.startWaitComplete();
+  }
+
+  tests[2] = function(testArray,testIndex) {
+  console.log("\nTest 3: Object bound function\n Should print 2 and 3 on seperate lines.");
+  var testObject = new TestObject(1);
+  var barrier = new Barrier(testComplete,null,"Test 3 Complete",testArray,testIndex);
+  barrier.registerFunction(testObject.testFunction,testObject,1);
+  barrier.registerFunction(testObject.testFunction,testObject,2);
+  barrier.startWaitComplete();
+ }
+
+ tests[3] = function(testArray,testIndex) {
+  console.log("\nTest 4: Object bound function, Object not passed\n Should print NaN.");
+  var testObject = new TestObject(1);
+  var barrier = new Barrier(testComplete,null,"Test 4 Complete",testArray,testIndex);
+  barrier.registerFunction(testObject.testFunction,null,1);
+  barrier.startWaitComplete();
+ }
+
+  tests[4] = function(testArray,testIndex) {
+  console.log("\nTest 5: register function after start wait\n Should print exception.");
+  var barrier = new Barrier(console.log,null,"Test 5 Complete");
+  barrier.registerFunction(testFunction,null,1);
+  barrier.startWaitComplete();
+  try {
+    barrier.registerFunction(testFunction,null,2);
+  } catch (ex) {
+    console.log(ex)
+  }
+  }
+
+  console.log("There are " + tests.length.toString() + " tests to run.");
+  tests[0](tests,0);
 }
 
- function testFunc(num,end)
- {
-     setTimeout(testFuncBody,10,num,end);
-
- }
-
- function testFuncBody(num,end)
- {
-     for (var i=0;i<num;i++)
-       {
-         console.log(i +"/" + num);
-       }
-      end();
- }
-
-
-function Barrier(doneFunction)
+function TestObject(number)
 {
-    this.functionCount = 0;
-    this.waiting = false;
-    this.allDoneFunction = doneFunction
-    this.oneDoneFunction = oneDoneFunctionClosure(this);
-    this.startWaitComplete = startWaitComplete;
-    this.registerFunction = registerFunction;
+  this.value = number;
+}
 
-    //not using slice function as is recommended against on MDN arguments article.
-    if (arguments.length===1)
-    {
-           this.allDoneArgs = [];
-    } else
-    {
-        var argArray = Array(arguments.length-1);
-        for ( var i = 1; i < (arguments.length);i++)
-        {
-              argArray[i-1] = arguments[i];
-        }
-        this.allDoneArgs= argArray;
+TestObject.prototype.testFunction = function(arg,end) {
+  testFunction(this.value+arg,end);
+}
+
+function testFunction(num,end)
+{
+  setTimeout(testFunctionSync,10,num,end);
+}
+
+function testFunctionSync(num,end)
+{
+  console.log(num);
+  end();
+}
+
+function testNoFunctionArguments(end)
+{
+  testFunction(0,end);
+}
+
+function testComplete(message,testArray,testIndex)
+{
+   console.log(message);
+   testIndex++;
+   if (testIndex < (testArray.length))
+   {
+      testArray[testIndex](testArray,testIndex);
    }
 }
-
-//expect first argument is function subsequent arguments are args for function excluding call back argument
-function registerFunction()
-{
-        if(this.waiting===true)
-        {
-                throw "SyncBarrier: You may not regsiter a function once you have started waiting for completion";
-        }
-
-        var funcToCall = arguments[0];
-        //convert arguments to array and take from 1 to end
-
-        //not using slice function as is recommended against on MDN arguments article.
-        var argArray = Array(arguments.length);
-        for ( var i = 1; i < (arguments.length);i++)
-        {
-              argArray[i-1] = arguments[i];
-        }
-        argArray[arguments.length-1] = this.oneDoneFunction;
-        this.functionCount++;
-
-         funcToCall.apply(null, argArray);
-
-}
-
-function startWaitComplete()
-{
-      if (this.functionCount===0)
-      {
-           this.allDoneFunction.apply(null,this.allDoneArgs);
-      } else {
-         this.waiting=true;
-      }
-
-}
-
-
-function oneDoneFunctionClosure(selfRef)
-{
-        return(function ()  {
-            selfRef.functionCount--;
-            if (selfRef.waiting)
-            {
-                 if (selfRef.functionCount===0)
-                  {
-                       selfRef.allDoneFunction.apply(null,selfRef.allDoneArgs);
-                  }
-            }
-      });
-}
+// </test>
